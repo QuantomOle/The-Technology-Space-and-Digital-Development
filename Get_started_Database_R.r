9987f6bc5edbdd9d0577392c8c0678a0b012958e
Ole <45040085+QuantomOle@users.noreply.github.com>
2022-06-27 16:31:00 +0200

    MongoDB Tutorial

---
+++ Get_started_Database_R.r
@@ -0,0 +1,23 @@
+#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%
+# Tutrial of how to get started with data on MongoDB #
+# 27-06-2022 | Ole Teutloff
+#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%
+#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%
+
+library(mongolite)
+library(tidyverse)
+
+# Place the .env file in the same folder as the R script
+readRenviron(".env")
+connection_string = Sys.getenv("CONNECTION_STRING")
+
+# Create connection to database
+SO_data = mongo(db="stackoverflow", collection="country_tags_2018", url=connection_string)
+
+# Collect the data from the database
+# inlcude columns by setting them to true
+# there are several filtering options available. See mongolite documentation for details
+
+data <- SO_data$find(fields = '{"Country": true, "tags" : true, "count" : true}')
+
+
