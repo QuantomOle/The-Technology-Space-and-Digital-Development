b55f91b6ddfb60c02cb4ab854ce3b87594f0b1e2
AlexandraRoko <a.rottenkolber@mpp.hertie-school.org>
2022-07-05 13:10:39 +0200

    -

--- code_alexandra/stackoverflow_network/01_data_in_shape.R
+++ code_alexandra/stackoverflow_network/01_data_in_shape.R
@@ -11,17 +11,38 @@
 library(mongolite)
 library(tidyverse)
 library(sys)
+library(dplyr)
 
 # Specify path to .env file
 readRenviron("/Users/alexandrarottenkolber/Documents/05_Spatial_Inequalities/Alexandra/.env")
 connection_string = Sys.getenv("CONNECTION_STRING")
 
 # Create connection to database
-SO_data = mongo(db="stackoverflow", collection="country_tags_2018", url=connection_string)
+CT_data = mongo(db="stackoverflow", collection="country_tags_2018", url=connection_string)
+SO_data = mongo(db="stackoverflow", collection="stackoverflow", url=connection_string)
 
 # Collect the data from the database
 # include columns by setting them to true
 # there are several filtering options available. See mongolite documentation for details
 
-data <- SO_data$find(fields = '{"Country": true, "tags" : true, "count" : true}')
+#data_country_tags <- SO_data$find(fields = '{"Country": true, "tags" : true, "count" : true}')
+data_SO <- SO_data$find(fields = '{"id": true, "Country": true, "tags" : true}')
+
+
+
+
+df <- as.data.frame(data)
+typeof(df)
+
+dataFrame <- data %>% 
+  as_tibble()
+typeof(dataFrame)
+
+colnames(data)
+
+data %>% filter("_id" == "61b778e8fc9434398d505a28")
+
+data 
+
+#df <- do.call(rbind.data.frame, data)
 
